ed1889ab4097a8f479b06a807d4e9d4b97e332e9
Kirk Stennett <kjsstennett@gmail.com>
2016-07-12 11:15:21 -0700

    minor bug fixes and log file output

--- run_test.sh
+++ run_test.sh
@@ -1,17 +1,19 @@
+# $1, $2, $3 all are command line arguments
 request=$1
 search_term=$2
 LOGFILE=$3
 
-
-#printf "Request: ${request}, search_term: ${search_term}, log: ${LOGFILE}\n"
-
 count=0
 while true
 do
+    # executes the request
     result=`eval $request`
-        
+       
+    # executes the grep command 
+    # stores result of grep in $grep_cmd
     grep_cmd=$(grep "${search_term}" ${LOGFILE})
 
+    # if grep returns result from search then break
     if [ "$grep_cmd" != "" ]
     then
         break;
@@ -21,4 +23,4 @@ do
     sleep 15s
 done
 
-printf "\nTook $((($count+1) * 15)) seconds to deploy.\n" > ${LOGFILE}
+printf "Took $((($count+1) * 15)) seconds to deploy.\n" > ${LOGFILE}
--- timing.sh
+++ timing.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-search_term=""
+
+# grep search term must be manually set
+search_term="mail"
 
 function run_loop {
     run_idx=0
     for i in "${request_list[@]}"
     do
         printf "Running: ${i}\n"
-        #./run_test.sh "${i}" "${search_term}" "${log_list[$run_idx]}"
+        # execute run_test.sh and pass command line arguments to it
+        # commands with & at the end are given a separate subprocess to run in
+        ./run_test.sh "${i}" "${search_term}" "${log_list[$run_idx]}" &
         run_idx=$((run_idx+1))
     done
     
@@ -25,40 +29,42 @@ function main {
     fi
         
     idx=0
+    # iterate through $path_list, each element in array is i
     for i in "${path_list[@]}"
     do
         if [ "${choice}" == "y" -o "${choice}" == "Y" ]
         then
-            request_list[$idx]="curl -i --header \"X-Forwarded-For: ${ip}\" \"${i}\" > ${i}.log &"
+            # request_list array passed individually to ./run_test.sh
+            request_list[$idx]="curl -i --header \"X-Forwarded-For: ${ip}\" \"${i}\" > ${i}.log"
         else
-            request_list[$idx]="curl -i \"${i}\" > ${i}.log &"
+            request_list[$idx]="curl -i \"${i}\" > ${i}.log"
         fi
+        
+        # list of log paths needed for grep in ./run_test.sh
         log_list[$idx]="${i}.log"
-
-        #printf "\n${request_list[$idx]}\n\n"
         idx=$((idx+1))
     done
+    
     echo "${result_list[@]}" 
     run_loop
 }
 
 function get_sites_from_file {
-    if [ "$1" == "" ]
-    then
-        printf "Enter a file path to read urls from: "
-        read path
-    else
-        path=$1
-    fi
+    printf "Enter a file path to read urls from: "
+    read path
 
     array_idx=0
+    
+    # reads line by line, stores result into $line
     while read -r line
     do
         path_list[$array_idx]=$line
         printf "Index: ${array_idx}, line: ${line}\n"
         array_idx=$((array_idx+1))
+    # $path is read from, passed as file input through <
     done < "$path"
     
+    # print path_list array
     echo "${path_list[@]}"
 }
 
